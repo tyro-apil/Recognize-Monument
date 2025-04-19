d2afcd3956975e5a67a2f6d37bde47ef636d822d
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-04-20 05:13:13 +0545

    fix: unique port + no reload to prevent milvus from crashing

--- main.py
+++ main.py
@@ -269,4 +269,4 @@ async def recognize_monument_json(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=False)
